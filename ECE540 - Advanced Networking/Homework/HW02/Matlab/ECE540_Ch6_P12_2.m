f8f58b7737cf9163a76d9a76bf9c2c0aff7fcedb
davidmkirby <david@davids-air.myfiosgateway.com>
2020-09-25 00:03:55 -0400

    Assignment updates

---
+++ ECE540 - Advanced Networking/Homework/HW02/Matlab/ECE540_Ch6_P12_2.m
@@ -0,0 +1,14 @@
+figure(2); clf
+p=0:0.001:1;
+N1=10;N2=30;N3=50;
+eff1=N1.*p.*(1-p).^(2.*(N1-1));
+eff2=N2.*p.*(1-p).^(2.*(N2-1));
+eff3=N3.*p.*(1-p).^(2.*(N3-1));
+hold;
+plot(p,eff1,'g','LineWidth',3)
+plot(p,eff2,'r','LineWidth',3)
+plot(p,eff3,'y','LineWidth',3)
+xlabel('Probability')
+ylabel('Efficiency')
+title('Pure ALOHA')
+legend('N = 10','N = 30','N = 50')
